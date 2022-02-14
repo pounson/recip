2f3e4907b89f7b7da0f1e4ca09847b67ef51bee3
pounson <fakhradg@gmail.com>
2022-02-14 14:49:40 +0300

    first commit

---
+++ recipes/calculator/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+def calculate_view(requests, dish_get):
+    servings = int(requests.GET.get('servings', 1))
+    send_dish = {}
+    for dish, ingredients in DATA.items():
+        if dish_get == dish:
+            for ing, count in ingredients.items():
+                send_dish[ing] = count * servings
+
+    context = {
+        'recipe': send_dish
+    }
+
+    return render(requests, 'calculator/index.html', context)
